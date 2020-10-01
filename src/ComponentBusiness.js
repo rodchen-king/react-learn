d6232391f679345fc28aeb135666a5f9f07af9f9
rodchen <rodchen_king@outlook.com>
2020-10-01 13:59:47 +0800

    1. 事件处理

--- src/ComponentBusiness.js
+++ src/ComponentBusiness.js
@@ -6,8 +6,20 @@ class ComponentBusiness extends React.Component {
     super(props);
 
     this.state = {
-      title: 'class组件'
+      title: 'class组件',
+      count: 0
     }
+  }
+
+  handleIncrement = (arg1, arg2, e) => {
+    debugger
+    console.log('child click')
+    e.stopPropagation()
+    e.preventDefault()
+  }
+
+  handleParentClick (e) {
+    console.log('parent click')
   }
 
   render() {
@@ -17,6 +29,9 @@ class ComponentBusiness extends React.Component {
         <header className="App-header">
           <p>
             {title}
+          </p>
+          <p onClick={(e) => this.handleParentClick(e)}>
+            <a href="www.baidu.com" onClick={this.handleIncrement.bind(this, 1, 2)}>点击</a>
           </p>
         </header>
       </div>
