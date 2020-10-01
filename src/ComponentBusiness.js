327305ba070769e03dbf35105b74ba1dd1fe2c2d
rodchen <rodchen_king@outlook.com>
2020-10-01 18:45:13 +0800

    1. 受控组件

--- src/ComponentBusiness.js
+++ src/ComponentBusiness.js
@@ -5,59 +5,29 @@ class ComponentBusiness extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      title: 'class组件',
-      count: 0,
-      numbers : [1, 2, 3, 4, 5]
-    }
+    this.state = {value: ''};
   }
 
-  handleAddNewItemClick = (e) => {
-    this.setState((preState) => {
-      return {
-        numbers: [preState.numbers.length + 1].concat(preState.numbers)
-      }
-    })
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
   }
 
-  handleDelteteItemClick = (deleteItem) => {
-    debugger
-    this.setState((preState) => {
-      return {
-        numbers: preState.numbers.filter(item => item !== deleteItem)
-      }
-    })
+  handleSubmit = (event) => {
+    alert('提交的名字: ' + this.state.value);
+    event.preventDefault();
   }
 
-  handleReverseClick = () => {
-    this.setState((preState) => {
-      return {
-        numbers: [...preState.numbers].reverse()
-      }
-    })
-  }
 
   render() {
-    const { title, numbers } = this.state;
-    const listItems = numbers.map((number, index) =>
-      <li key={index.toString()}> <span>{number}</span><input /><button onClick={() => {this.handleDelteteItemClick(number)}}>delete</button></li>
-    );
     return (
       <div className="App">
-        <div className="App-header">
-          <p>
-            {title}
-          </p>
-          <ul>{listItems}</ul>
-        </div>
-        <div>
-          <button onClick={this.handleAddNewItemClick}>
-            头部添加一个item
-          </button>
-          <button onClick={this.handleReverseClick}>
-            反序数组
-          </button>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            名字:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="提交" />
+        </form>
       </div>
     );
   }
