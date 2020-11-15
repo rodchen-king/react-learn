4adfc3acefb1f3cc72b53cde3f5bfd1157cb5da0
rodchen <rodchen_king@outlook.com>
2020-11-15 23:25:32 +0800

    1. Profiler

--- src/lazy/OtherComponent.jsx
+++ src/lazy/OtherComponent.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const OtherComponent = () => {
-  return (
-    <div>
-      OtherComponent
-    </div>
-  );
+class OtherComponent extends React.Component {
+  componentDidMount() {
+    console.log('compontDidMount')
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+
+  render() {
+    return (
+      <div>
+        OtherComponent
+      </div>
+    );
+  }
 }
 
 export default OtherComponent;
--- src/lazy/index.jsx
+++ src/lazy/index.jsx
@@ -1,17 +1,48 @@
-import React, { Suspense } from 'react';
-
-const OtherComponent1 = React.lazy(() => new Promise(resolve =>
-  setTimeout(() => resolve(import('./OtherComponent') ),
-    3000
-  )
-));
+import React, { useEffect } from 'react';
+import { render } from "react-dom";
+import { unstable_trace as trace, unstable_Profiler as Profiler } from "scheduler/tracing";
+import OtherComponent from './OtherComponent'
 
 const MyComponent = () => {
+  const callback = (
+      id, // 发生提交的 Profiler 树的 “id”
+      phase, // "mount" （如果组件树刚加载） 或者 "update" （如果它重渲染了）之一
+      actualDuration, // 本次更新 committed 花费的渲染时间
+      baseDuration, // 估计不使用 memoization 的情况下渲染整颗子树需要的时间
+      startTime, // 本次更新中 React 开始渲染的时间
+      commitTime, // 本次更新中 React committed 的时间
+      interactions // 属于本次更新的 interactions 的集合
+    ) => {
+      console.log(`id: ${id}`)
+      console.log(`phase: ${phase}`)
+      console.log(`actualDuration: ${actualDuration}`)
+      console.log(`baseDuration: ${baseDuration}`)
+      console.log(`startTime: ${startTime}`)
+      console.log(`commitTime: ${commitTime}`)
+      console.log(`interactions: ${JSON.stringify(interactions)}`)
+
+  }
+
+  const useDidMount = fn => useEffect(() => fn && fn(), []);
+
+  useDidMount(() => {
+    trace("initial render", performance.now(), () =>
+      render(
+        <Profiler id="Navigation" onRender={callback}>
+          <OtherComponent key='1' />
+        </Profiler>,
+        document.getElementById('li3')
+      )
+    );
+  })
+
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <OtherComponent1 />
-      </Suspense>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li id='li3'></li>
+      </ul>
     </div>
   );
 }
